009f504bdea969386a9bbc528593d3cbe92b6df0
chiefwithbeef <210036@ndhs.org>
2019-04-17 09:27:55 -0700

    initial commit with actual stuff

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from keras.datasets import mnist
+import matplotlib
+import keras
+
+image_height, image_width = 28, 28
 
 if __name__ == "__main__":
-	pass
+
+	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	x_train = x_train.reshape(x_train.shape[0], 1, image_width, image_height)
+	y_train = y_train.reshape(y_train.shape[0], 1, image_width, image_height)
