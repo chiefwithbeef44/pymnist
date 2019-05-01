a0b89065ea1e7df06fee1e7c3bef7a355983671c
chiefwithbeef44 <34226571+chiefwithbeef44@users.noreply.github.com>
2019-05-01 10:01:36 -0700

    Update modelTester.py

--- model/modelTester.py
+++ model/modelTester.py
@@ -1,3 +1,4 @@
 # noinspection PyPep8Naming
 def evaluateAccuracy(model, x_train, y_train, batch_size, epochs, x_test, y_test):
 	model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+	return model.evaluate(x_test, y_test, verbose=0)
